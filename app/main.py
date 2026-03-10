3d1778bd367c909a19a6560ffcf550af0548f690
Raunak-Dehankar <raunak.42.dehankar@gmail.com>
2026-03-10 19:50:34 +0530

    modified the database to use Sqlite instead of memory

--- app/main.py
+++ app/main.py
@@ -1,26 +1,37 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
 from fastapi.responses import RedirectResponse
-from app.url_service import create_short_url, get_original_url
+from sqlalchemy.orm import Session
+
+from app.database import get_db
 from app.schemas import URLRequest
-from fastapi import HTTPException
+from app.url_service import URLService
+from app.database import get_db, engine
+from app.models import Base
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"message": "URL Shortener API"}
-
 
 @app.post("/shorten")
-def shorten_url(request: URLRequest):
-    code = create_short_url(request.url)
-    return {"short_url": f"http://localhost:8000/{code}"}
+def shorten_url(request: URLRequest, db: Session = Depends(get_db)):
+
+    service = URLService(db)
+
+    url = service.create_short_url(request.url)
+
+    return {
+        "short_url": f"http://localhost:8000/{url.short_code}"
+    }
+
 
 @app.get("/{short_code}")
-def redirect(short_code: str):
-    original_url = get_original_url(short_code)
+def redirect(short_code: str, db: Session = Depends(get_db)):
 
-    if not original_url:
+    service = URLService(db)
+    url = service.get_original_url(short_code)
+
+    if not url:
         raise HTTPException(status_code=404, detail="URL not found")
 
-    return RedirectResponse(original_url)
+    return RedirectResponse(url=url.original_url, status_code=307)
--- app/url_service.py
+++ app/url_service.py
@@ -1,11 +1,33 @@
+from sqlalchemy.orm import Session
+from app.models import URL
 from app.utils import generate_short_code
 
-url_store = {}
 
-def create_short_url(original_url):
-    code = generate_short_code()
-    url_store[code] = original_url
-    return code
+class URLService:
 
-def get_original_url(code):
-    return url_store.get(code)
+    def __init__(self, db: Session):
+        self.db = db
+
+    def create_short_url(self, original_url: str):
+
+        code = generate_short_code()
+
+        db_url = URL(
+            original_url=original_url,
+            short_code=code
+        )
+
+        self.db.add(db_url)
+        self.db.commit()
+        self.db.refresh(db_url)
+
+        return db_url
+
+
+    def get_original_url(self, code: str):
+
+        return (
+            self.db.query(URL)
+            .filter(URL.short_code == code)
+            .first()
+        )
